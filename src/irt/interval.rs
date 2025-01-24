5cef1a612c8057ef50a5584ea0852df0d8f1e538
Shybert <25749775+shybert@users.noreply.github.com>
2025-01-24 11:07:22 +0100

    Interval utilities

--- src/irt/interval.rs
+++ src/irt/interval.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, Copy)]
 pub struct Interval {
     pub min: f32,
     pub max: f32,
@@ -5,6 +6,18 @@ pub struct Interval {
 impl Interval {
     pub fn new(min: f32, max: f32) -> Self {
         return Self { min, max };
+    }
+
+    pub fn empty() -> Self {
+        return Self::new(f32::INFINITY, f32::NEG_INFINITY);
+    }
+
+    pub fn from_intervals(a: &Self, b: &Self) -> Self {
+        return Self::new(a.min.min(b.min), a.max.max(b.max));
+    }
+
+    pub fn size(&self) -> f32 {
+        return self.max - self.min;
     }
 
     pub fn surrounds(&self, x: f32) -> bool {
@@ -18,5 +31,11 @@ impl Interval {
             return self.max;
         }
         return x;
+    }
+
+    pub fn expand(&self, delta: f32) -> Self {
+        let padding = delta / 2.;
+
+        return Interval::new(self.min - padding, self.max + padding);
     }
 }
