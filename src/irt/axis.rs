9af4f5ee6afc080fba0f10508fdb114b1eecc7d9
Shybert <25749775+shybert@users.noreply.github.com>
2025-01-31 16:19:49 +0100

    No need for a crate for a simple enum iterator

--- src/irt/axis.rs
+++ src/irt/axis.rs
@@ -1,8 +1,11 @@
-use strum::EnumIter;
-
-#[derive(EnumIter, Clone, Copy)]
+#[derive(Clone, Copy)]
 pub enum Axis {
     X,
     Y,
     Z,
+}
+impl Axis {
+    pub fn iter() -> impl Iterator<Item = Axis> {
+        return [Axis::X, Axis::Y, Axis::Z].into_iter();
+    }
 }
