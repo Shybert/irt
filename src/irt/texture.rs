a6018d1dbec36f208824baf1fd9b5aed319debe1
Sivert Olstad <sivert.olstad@protonmail.com>
2025-03-27 17:16:00 +0100

    Simple checkered texture

--- src/irt/texture.rs
+++ src/irt/texture.rs
@@ -12,3 +12,33 @@ impl Texture for Color {
         return *self;
     }
 }
+
+#[derive(Debug)]
+pub struct CheckeredTexture {
+    scale_inverted: f32,
+    even: Box<dyn Texture>,
+    odd: Box<dyn Texture>,
+}
+impl CheckeredTexture {
+    pub fn new(scale: f32, even: Box<dyn Texture>, odd: Box<dyn Texture>) -> Self {
+        return Self {
+            scale_inverted: 1. / scale,
+            even,
+            odd,
+        };
+    }
+}
+impl Texture for CheckeredTexture {
+    fn value(&self, u: f32, v: f32, point: Point) -> Color {
+        let x: i32 = (point.x * self.scale_inverted).floor() as i32;
+        let y: i32 = (point.y * self.scale_inverted).floor() as i32;
+        let z: i32 = (point.z * self.scale_inverted).floor() as i32;
+
+        let is_even = (x + y + z) % 2 == 0;
+        return if is_even {
+            self.even.value(u, v, point)
+        } else {
+            self.odd.value(u, v, point)
+        };
+    }
+}
