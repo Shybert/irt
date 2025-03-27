c392f88ceeda6bf9f0a3399997b580a4907feaa1
Sivert Olstad <sivols01@e127551.arm.com>
2025-03-27 15:55:47 +0100

    Remember that Rust is an amazing programming language

--- src/irt/texture.rs
+++ src/irt/texture.rs
@@ -7,18 +7,8 @@ pub trait Texture: Debug + Sync + Send {
     fn value(&self, u: f32, v: f32, point: Point) -> Color;
 }
 
-#[derive(Debug)]
-pub struct SolidColor {
-    albedo: Color,
-}
-impl SolidColor {
-    pub fn new(albedo: Color) -> Self {
-        return Self { albedo };
-    }
-}
-
-impl Texture for SolidColor {
+impl Texture for Color {
     fn value(&self, _u: f32, _v: f32, _point: Point) -> Color {
-        return self.albedo;
+        return *self;
     }
 }
