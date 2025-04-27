9eec74f63ca76f03644ccc9972f3766957ebf134
Sivert Olstad <sivert.olstad@protonmail.com>
2025-04-27 09:45:15 +0200

    Introduce function for approximate floating-point equality
    This also introduces an epsilon constant that can be used other places
    where accommodating for floating-point rounding error is useful. This is
    a TODO
    
    Taken from my previous Rust ray-tracer, `sorb-rust`

--- src/irt/utils.rs
+++ src/irt/utils.rs
@@ -5,3 +5,9 @@ pub fn degrees_to_radians(degrees: f32) -> f32 {
 pub fn lerp(v0: f32, v1: f32, t: f32) -> f32 {
     return v0 + t * (v1 - v0);
 }
+
+/// Constant used to accommodate for floating-point rounding error.
+pub const EPSILON: f32 = 0.00001;
+pub fn approx_equals(a: f32, b: f32) -> bool {
+    return (a - b).abs() < EPSILON;
+}
